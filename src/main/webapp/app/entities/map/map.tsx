590e59f7fa2cd88e3d0b0293d4f3207991ea00a5
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-04-19 21:07:54 -0400

    sprintly logo added, button styling

--- src/main/webapp/app/entities/map/map.tsx
+++ src/main/webapp/app/entities/map/map.tsx
@@ -56,17 +56,15 @@ export const Map = (props: RouteComponentProps<{ url: string }>) => {
                 <Gmap mapType={google.maps.MapTypeId.ROADMAP} mapTypeControl={true}/>
               )}
       </div>
-      <button onClick={getLocation}>Get Location</button>
+      <div className="container-fluid d-flex">
+        <button className="btn btn-primary justify-content-center" onClick={getLocation}>Start Run</button>
+        <button className="btn btn-primary justify-content-center" onClick={getLocation}>Stop Run</button>
+        </div>
       <h1>Coordinates</h1>
       <p>{status}</p>
       {lat && <p>Latitude: {lat}</p>}
       {lng && <p>Longitude: {lng}</p>}
-      <button 
-        style={{ backgroundColor: "blue" }}
-        className="startBtn">Start Run</button>
-       <button 
-        style={{ backgroundColor: "blue" }}
-      className="startBtn">Stop Run</button>
+      
       <h2 id="map-heading" data-cy="MapHeading">
         Maps
         <div className="d-flex justify-content-end">
