88ab847fe2d5c84a884e864a8fbf327813f17bf8
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-04-24 13:52:23 -0400

    removed table from maps

--- src/main/webapp/app/entities/map/map.tsx
+++ src/main/webapp/app/entities/map/map.tsx
@@ -95,57 +95,58 @@ export const Map = (props: RouteComponentProps<{ url: string }>) => {
           {endLat && <p>End Latitude: {endLat}</p>}
           {endLng && <p>End Longitude: {endLng}</p>}
           {timeEnd && <p> End Time Stamp {timeEnd}</p>}
-     <h2 id="map-heading" data-cy="MapHeading">
-        Maps
-        <div className="d-flex justify-content-end">
-          <Button className="me-2" color="info" onClick={handleSyncList} disabled={loading}>
-            <FontAwesomeIcon icon="sync" spin={loading} /> Refresh List
-          </Button>
-          <Link to="/map/new" className="btn btn-primary jh-create-entity" id="jh-create-entity" data-cy="entityCreateButton">
-            <FontAwesomeIcon icon="plus" />
-            &nbsp; Create new Map
-          </Link>
-        </div>
-      </h2>
-      <div className="table-responsive">
-        {mapList && mapList.length > 0 ? (
-          <Table responsive>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>
-              {mapList.map((map, i) => (
-                <tr key={`entity-${i}`} data-cy="entityTable">
-                  <td>
-                    <Button tag={Link} to={`/map/${map.id}`} color="link" size="sm">
-                      {map.id}
-                    </Button>
-                  </td>
-                  <td className="text-end">
-                    <div className="btn-group flex-btn-group-container">
-                      <Button tag={Link} to={`/map/${map.id}`} color="info" size="sm" data-cy="entityDetailsButton">
-                        <FontAwesomeIcon icon="eye" /> <span className="d-none d-md-inline">View</span>
-                      </Button>
-                      <Button tag={Link} to={`/map/${map.id}/edit`} color="primary" size="sm" data-cy="entityEditButton">
-                        <FontAwesomeIcon icon="pencil-alt" /> <span className="d-none d-md-inline">Edit</span>
-                      </Button>
-                      <Button tag={Link} to={`/map/${map.id}/delete`} color="danger" size="sm" data-cy="entityDeleteButton">
-                        <FontAwesomeIcon icon="trash" /> <span className="d-none d-md-inline">Delete</span>
-                      </Button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        ) : (
-          !loading && <div className="alert alert-warning">No Maps found</div>
-        )}
-      </div>
-    </div>
+          </div>
+//      <h2 id="map-heading" data-cy="MapHeading">
+//         Maps
+//         <div className="d-flex justify-content-end">
+//           <Button className="me-2" color="info" onClick={handleSyncList} disabled={loading}>
+//             <FontAwesomeIcon icon="sync" spin={loading} /> Refresh List
+//           </Button>
+//           <Link to="/map/new" className="btn btn-primary jh-create-entity" id="jh-create-entity" data-cy="entityCreateButton">
+//             <FontAwesomeIcon icon="plus" />
+//             &nbsp; Create new Map
+//           </Link>
+//         </div>
+//       </h2>
+//       <div className="table-responsive">
+//         {mapList && mapList.length > 0 ? (
+//           <Table responsive>
+//             <thead>
+//               <tr>
+//                 <th>ID</th>
+//                 <th />
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {mapList.map((map, i) => (
+//                 <tr key={`entity-${i}`} data-cy="entityTable">
+//                   <td>
+//                     <Button tag={Link} to={`/map/${map.id}`} color="link" size="sm">
+//                       {map.id}
+//                     </Button>
+//                   </td>
+//                   <td className="text-end">
+//                     <div className="btn-group flex-btn-group-container">
+//                       <Button tag={Link} to={`/map/${map.id}`} color="info" size="sm" data-cy="entityDetailsButton">
+//                         <FontAwesomeIcon icon="eye" /> <span className="d-none d-md-inline">View</span>
+//                       </Button>
+//                       <Button tag={Link} to={`/map/${map.id}/edit`} color="primary" size="sm" data-cy="entityEditButton">
+//                         <FontAwesomeIcon icon="pencil-alt" /> <span className="d-none d-md-inline">Edit</span>
+//                       </Button>
+//                       <Button tag={Link} to={`/map/${map.id}/delete`} color="danger" size="sm" data-cy="entityDeleteButton">
+//                         <FontAwesomeIcon icon="trash" /> <span className="d-none d-md-inline">Delete</span>
+//                       </Button>
+//                     </div>
+//                   </td>
+//                 </tr>
+//               ))}
+//             </tbody>
+//           </Table>
+//         ) : (
+//           !loading && <div className="alert alert-warning">No Maps found</div>
+//         )}
+//       </div>
+//     </div>
   );
 };
 
